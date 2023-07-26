ef54bc1675bad7d9b0a3842f91c859205531921a
CHIEF1K <quaysontitus@gmail.com>
2023-07-26 20:14:49 +0000

    add updates to USSDController

--- app/Http/Controllers/USSDController.php
+++ app/Http/Controllers/USSDController.php
@@ -64,7 +64,7 @@ class USSDController extends Controller
                     $json_data = array(
                         "app_id" => $app_id,
                         "app_key" => $app_key,
-                        "email" => $merchant_id, 
+                        "name" => $merchant_id,
                         "FeeTypeCode" => "GENERALPAYMENT",
                         "mobile" => $mobile,
                         "currency" => "GHS",
