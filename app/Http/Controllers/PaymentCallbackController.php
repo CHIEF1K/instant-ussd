ba33fdc21aa2e528a2d110884f30bebfca780b57
Titus Nana Quayson <126072903+CHIEF1K@users.noreply.github.com>
2023-08-05 10:14:27 +0000

    Update PaymentCallbackController.php

--- app/Http/Controllers/PaymentCallbackController.php
+++ app/Http/Controllers/PaymentCallbackController.php
@@ -30,10 +30,15 @@ class PaymentCallbackController extends Controller
                 $amount = $payment_transaction->amount;
                 $mobile = $payment_transaction->resource_id;
                 $merchant_id = $payment_transaction -> merchants_name;
+                $status_message = $payment_transaction -> status_message;
+                $status_code = $payment_transaction -> status_code;
+                $transaction_no = $payment_transaction -> transaction_no;
+
+
 
                 if ($transaction_type == "payment") {
                     // Handle successful payment
-                    $this->handleSuccessfulPayment($transaction_id, $order_id, $mobile, $amount, $merchant_id);
+                    $this->handleSuccessfulPayment($transaction_id, $order_id, $mobile, $amount, $merchant_id,$status_code,$status_message,$transaction_no);
 
                     return response('Done');
                 }
@@ -43,14 +48,19 @@ class PaymentCallbackController extends Controller
         return response('Failed', 400);
     }
 
-    private function handleSuccessfulPayment($transaction_id,$order_id, $mobile, $amount, $merchant_id)
+    private function handleSuccessfulPayment($transaction_id,$order_id, $mobile, $amount, $merchant_id,$status_code,$status_message,$transaction_no)
     {
         // Insert payment record
         DB::table('payments')->insert([
             'order_id' => $order_id,
             'phone_number' => $mobile,
             'amount' => $amount,
-            'date_updated' => now()
+            'date_updated' => now(),
+            'status_message' => $status_message,
+            'status_code' => $status_code,
+            'transaction_no' => $transaction_no,
+            'merchant_id'=> $merchant_id,
+            'id' => $transaction_id
         ]);
 
         // Get the merchant's phone number
