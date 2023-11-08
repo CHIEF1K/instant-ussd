ef4e31c82dd1a46eed6400f8a6633c082e91add8
Titus Nana Quayson <126072903+CHIEF1K@users.noreply.github.com>
2023-11-08 07:32:18 +0000

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\USSDController;
 use App\Http\Controllers\PaymentCallbackController;
 use App\Http\Controllers\USSDReferenceController; 
+use App\Http\Controllers\Peer2PeerController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -24,3 +26,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('/ussd/{merchant_id}', [USSDController::class, 'handleUSSDRequest']);
 Route::post('/payment-callback', [PaymentCallbackController::class, 'handlePaymentCallback']);
 Route::post('/handle-reference/{merchant_id}', [USSDReferenceController::class, 'handleReferenceRequest']); 
+Route::post('/p2p/{merchant_id}', [Peer2PeerController::class, 'handleP2PRequest']);
